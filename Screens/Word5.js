2b137ee7963ec6192e58da75f53d6b9c53899731
Nico <nicolas.carrasco.m@.com>
2022-04-19 12:52:54 -0400

    custom hook useWord

--- Screens/Word5.js
+++ Screens/Word5.js
@@ -1,76 +1,12 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  ScrollView,
-  TextInput,
-  Button,
-} from "react-native";
-import React, { useRef, useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import React from "react";
 
-import { dictionary, board } from "../src/data";
 import Boton from "../src/components/Boton";
 import { styles } from "../src/theme/appTheme";
-
-//boardRandom = board.sort(() => Math.random() - 0.5);
+import { useWord } from "../src/hooks/useWord";
 
 const Word5 = () => {
-  function listToMatrix(list, elementsPerSubArray) {
-    var matrix = [],
-      i,
-      k;
-    for (i = 0, k = -1; i < list.length; i++) {
-      if (i % elementsPerSubArray === 0) {
-        k++;
-        matrix[k] = [];
-      }
-      matrix[k].push(list[i]);
-    }
-    return matrix;
-  }
-
-  var matrix = listToMatrix(
-    board.sort(() => Math.random() - 0.5),
-    4
-  );
-  const [matrixRandom, setMatrizRandom] = useState(
-    listToMatrix(
-      board.sort(() => Math.random() - 0.5),
-      4
-    )
-  );
-  const [word, setWord] = useState("");
-  const [dicc, setDicc] = useState(dictionary);
-  const [valid, setValid] = useState("");
-
-  useEffect(() => {
-    if (dicc.find((element) => element === word.toLowerCase()) === undefined) {
-      console.log("palabra no encontrada");
-    } else {
-      const newDicc = dicc.filter((x) => x !== word.toLowerCase());
-      console.log("newdicc " + newDicc);
-      setValid("Valid");
-
-      setDicc(newDicc);
-
-      setTimeout(() => {
-        alert("Palabra " + word + " encontrada. Busca una nueva palabra");
-      }, 500);
-
-      setTimeout(() => {
-        setWord("");
-        setMatrizRandom(matrix);
-        setValid("");
-      }, 2000);
-    }
-    console.log("word " + word);
-    // setLista(board);
-    // setMatrizRandom(matrix);
-  }, [word]);
-
-  const findWord = (x) => setWord(word + x);
+  const { word, setWord, findWord, matrixRandom, valid } = useWord();
   return (
     <View>
       <View style={styles.contentClear}>
---
+++ src/hooks/useWord.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { dictionary, board } from "../data";
+
+export const useWord = () => {
+  function listToMatrix(list, elementsPerSubArray) {
+    var matrix = [],
+      i,
+      k;
+    for (i = 0, k = -1; i < list.length; i++) {
+      if (i % elementsPerSubArray === 0) {
+        k++;
+        matrix[k] = [];
+      }
+      matrix[k].push(list[i]);
+    }
+    return matrix;
+  }
+
+  var matrix = listToMatrix(
+    board.sort(() => Math.random() - 0.5),
+    4
+  );
+  const [matrixRandom, setMatrizRandom] = useState(
+    listToMatrix(
+      board.sort(() => Math.random() - 0.5),
+      4
+    )
+  );
+  const [word, setWord] = useState("");
+  const [dicc, setDicc] = useState(dictionary);
+  const [valid, setValid] = useState("");
+
+  useEffect(() => {
+    if (dicc.find((element) => element === word.toLowerCase()) === undefined) {
+      console.log("palabra no encontrada");
+    } else {
+      const newDicc = dicc.filter((x) => x !== word.toLowerCase());
+      console.log("newdicc " + newDicc);
+      setValid("Valid");
+
+      setDicc(newDicc);
+
+      setTimeout(() => {
+        alert("Palabra " + word + " encontrada. Busca una nueva palabra");
+      }, 500);
+
+      setTimeout(() => {
+        setWord("");
+        setMatrizRandom(matrix);
+        setValid("");
+      }, 2000);
+    }
+    console.log("word " + word);
+    // setLista(board);
+    // setMatrizRandom(matrix);
+  }, [word]);
+
+  const findWord = (x) => setWord(word + x);
+
+  return { word, setWord, findWord, matrixRandom, valid };
+};
